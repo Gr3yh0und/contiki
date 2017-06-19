4cdaf3f04232a8f5fee91e26972af54b815edb81
Michael Morscher <ich@morschi.com>
2017-06-19 22:42:20 +0200

    Alter Makefile, Build script and new YaCoAP library

--- examples/coaps/build_contiki.sh
+++ examples/coaps/build_contiki.sh
@@ -1,24 +1,45 @@
 #!/bin/bash
 
 # Configuration, ToDo: Remove hard coding
-DIRECTORY="obj_cc2538dk"
+DIRECTORY_CC2538DK="obj_cc2538dk"
+DIRECTORY_OPENMOTE="obj_openmote-cc2538"
 THREADS=2
+DELETION=0
 
 # Delete old object files
-if [ -d "$DIRECTORY" ]; then
-	echo "Removing old object files..."
-	rm -r obj_cc2538dk
+if [ $DELETION == 1 ]; then
+	echo "Removing possible old object files..."
+	if [ -d "$DIRECTORY_CC2538DK" ]; then
+		echo "CC2538DK"
+		rm -r $DIRECTORY_CC2538DK
+	fi
+	if [ -d "$DIRECTORY_OPENMOTE" ]; then
+		echo "OpenMote"
+		rm -r $DIRECTORY_OPENMOTE
+	fi
 fi
 
 # Start new build
 echo "Starting new build..."
-make -j$THREADS
+echo "CC2538DK:"
+make -j$THREADS TARGET=cc2538dk
 if [[ $? != 0 ]]; then
 	exit 255
+else
+	mv coaps.elf coaps-contiki-cc2538dk.elf
+	mv coaps.bin coaps-contiki-cc2538dk.bin
+fi
+echo "OpenMote:"
+make -j$THREADS TARGET=openmote-cc2538
+if [[ $? != 0 ]]; then
+	exit 255
+else
+	mv coaps.elf coaps-contiki-openmote.elf
+	mv coaps.bin coaps-contiki-openmote.bin
 fi
 
 # Get size of new build
-OUTPUT_NEW="$(arm-none-eabi-size coaps.elf)"
+OUTPUT_NEW="$(arm-none-eabi-size coaps-contiki-cc2538dk.elf)"
 RE_NEW="([0-9]{3,6})\s*([0-9]{3,6})\s*([0-9]{3,6})\s*([0-9]{3,6})"
 [[ $OUTPUT_NEW =~ $RE_NEW ]]
 	TEXT_NEW=${BASH_REMATCH[1]}
@@ -42,6 +63,7 @@ DATA_DIF=`expr $DATA_NEW - $DATA_OLD`
 DEC_DIF=`expr $DEC_NEW - $DEC_OLD`
 
 # Print results
+echo ""
 echo "Old: TEXT($TEXT_OLD), BSS($BSS_OLD), DATA($DATA_OLD), DEC($DEC_OLD)"
 echo "New: TEXT($TEXT_NEW), BSS($BSS_NEW), DATA($DATA_NEW), DEC($DEC_NEW)"
 echo "Dif: TEXT($TEXT_DIF), BSS($BSS_DIF), DATA($DATA_DIF), DEC($DEC_DIF)"
